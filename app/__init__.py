b93904582d136c58e3ab11e2a70262896cc88874
mattgogerly <matthewgogerly@gmail.com>
2025-01-12 00:12:28 +0000

    chore(lint): fix spacing of imports

--- app/__init__.py
+++ app/__init__.py
@@ -21,23 +21,15 @@ def create_app():
         db.create_all()
 
     from .web.home import home_bp
-
-    app.register_blueprint(home_bp)
-
     from .web.accounts import accounts_bp
-
-    app.register_blueprint(accounts_bp, url_prefix="/accounts")
-
     from .web.pots import pots_bp
-
-    app.register_blueprint(pots_bp, url_prefix="/pots")
-
     from .web.auth import auth_bp
-
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-
     from .web.settings import settings_bp
 
+    app.register_blueprint(home_bp)
+    app.register_blueprint(accounts_bp, url_prefix="/accounts")
+    app.register_blueprint(pots_bp, url_prefix="/pots")
+    app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(settings_bp, url_prefix="/settings")
 
     scheduler.init_app(app)
