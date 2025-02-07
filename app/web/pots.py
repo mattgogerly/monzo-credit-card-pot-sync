cdcc35b816f4e35182cd94d00faa4288c37cc280
mattgogerly <matthewgogerly@gmail.com>
2025-02-07 17:46:47 +0000

    fix(pots): add more logging to debug issues fetching pots

--- app/web/pots.py
+++ app/web/pots.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from sqlalchemy.exc import NoResultFound
 
@@ -7,19 +9,27 @@ from app.models.account_repository import SqlAlchemyAccountRepository
 
 pots_bp = Blueprint("pots", __name__)
 
+log = logging.getLogger("pots")
 account_repository = SqlAlchemyAccountRepository(db)
 
 
 @pots_bp.route("/", methods=["GET"])
 def index():
     try:
+        log.info("Retrieving Monzo account")
         monzo_account: MonzoAccount = account_repository.get_monzo_account()
+
+        log.info("Retrieving pots for Monzo account")
         pots = monzo_account.get_pots()
     except NoResultFound:
         flash("You need to connect a Monzo account before you can view pots", "error")
         pots = []
 
+    log.info(f"Retrieved {len(pots)} pots from Monzo")
+
+    log.info("Retrieving credit card accounts")
     accounts = account_repository.get_credit_accounts()
+    
     return render_template("pots/index.html", pots=pots, accounts=accounts)
 
 
