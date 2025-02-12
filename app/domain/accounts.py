09bd233b6c21526549a25a0621789a0cee3c0a51
Martin Adams <6276146+martadams89@users.noreply.github.com>
2025-02-12 16:35:13 +0000

    adjusted balance (#10)
    * adjusted balance
    
    * pednign credits

--- app/domain/accounts.py
+++ app/domain/accounts.py
@@ -252,11 +252,9 @@ class TrueLayerAccount(Account):
                 log.info(f"Pending Credits: £{pending_credits}")
                 log.info(f"Total Balance: £{adjusted_balance}")
 
-                adjusted_balance -= pending_credits  # Deduct credits from balance
-
-                balance = adjusted_balance
+                balance -= pending_credits  # Deduct credits from balance
 
             total_balance += balance
 
-        log.info(f"Total balance calculated: {total_balance}")
+        log.info(f"Total balance calculated: {total_balance / 100:.2f}")
         return int(total_balance * 100)  # Convert balance to pence
