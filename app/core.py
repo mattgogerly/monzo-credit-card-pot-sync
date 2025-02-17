023c13b5027dad4ac3891cf33c976439d8088819
martinadamsUL <martin.adams@ul.com>
2025-02-17 12:19:53 +0000

    Add settings import to core module

--- app/core.py
+++ app/core.py
@@ -3,6 +3,7 @@ from sqlalchemy.exc import NoResultFound
 
 from app.domain.accounts import MonzoAccount, TrueLayerAccount
 from app.errors import AuthException
+from app.domain.settings import Setting
 from app.extensions import db, scheduler
 from app.models.account_repository import SqlAlchemyAccountRepository
 from app.models.setting_repository import SqlAlchemySettingRepository
