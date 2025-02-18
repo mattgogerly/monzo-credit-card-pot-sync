d069f2329cff44d44f07adb42fe6dbae88a9d8f2
martinadamsUL <martin.adams@ul.com>
2025-02-18 12:32:30 +0000

    Remove custom JSON type and simplify prev_balance field to integer

--- app/models/account.py
+++ app/models/account.py
@@ -1,21 +1,6 @@
 from app.extensions import db
 import json
 from sqlalchemy.types import TypeDecorator, JSON
-from sqlalchemy.ext.mutable import MutableDict
-
-# Custom JSON type that coerces string values to a dict.
-class CoerceJSON(TypeDecorator):
-    impl = JSON
-
-    def process_result_value(self, value, dialect):
-        if value is None or value == "":
-            return {}
-        if isinstance(value, str):
-            try:
-                return json.loads(value)
-            except Exception:
-                return {}
-        return value
 
 class AccountModel(db.Model):
     type = db.Column(db.String(150), primary_key=True)
@@ -25,5 +10,4 @@ class AccountModel(db.Model):
     token_expiry = db.Column(db.Integer)
     account_id = db.Column(db.String(150), nullable=True)
     cooldown_until = db.Column(db.Integer, nullable=True)
-    # Replace prev_balances JSON field with a simple integer field.
     prev_balance = db.Column(db.Integer, default=0)
